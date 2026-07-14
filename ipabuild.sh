f638ef8be74e33f58f39522e24a76e43fb2f7956
adm00a1a-cloud <adm00a1a@gmail.com>
2026-07-14 05:01:35 +0300

    Fix GitHub Actions workflow: use working-directory: Lara3-fixed

--- ipabuild.sh
+++ ipabuild.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-set -eo pipefail
-cd "$(dirname "$0")"
-
-# The project is inside Lara3-fixed/
-cd Lara3-fixed
-
-exec ./ipabuild.sh "$@"
+set -e
+cd "$(dirname "$0")/Lara3-fixed"
+exec bash ./ipabuild.sh "$@"
